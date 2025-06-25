e45c7820a23eee9c61526794120a6b193f66d075
nicoargiolas <argiolas.nicola00@gmail.com>
2025-06-25 13:59:15 +0200

    Snack 3, 4 e 5

--- testsnacks.js
+++ testsnacks.js
@@ -6,6 +6,7 @@ function getInitials(completeName) {
     return completeName.trim()[0].toUpperCase() + (completeName.trim().split(" "))[1][0].toUpperCase()
 }
 
+
 // 🏆 Snack 2
 // Creare un test che verifichi la seguente descrizione:
 // 👉 "La funzione createSlug restituisce una stringa in lowercase."
@@ -14,7 +15,37 @@ function createSlug(string) {
     return string.replaceAll(" ", "-").toLowerCase();
 }
 
+
+// 🏆 Snack 3
+// Creare un test che verifichi la seguente descrizione:
+// 👉 "La funzione average calcola la media aritmetica di un array di numeri."
+
+function average(array) {
+    const somma = array.reduce((acc, curr) => {
+        return acc + curr
+    }, 0)
+    return somma / array.length
+}
+
+
+// 🏆 Snack 5
+// Creare un test che verifichi la seguente descrizione:
+// 👉 "La funzione isPalindrome verifica se una stringa è un palindromo."
+
+function isPalindrome(string) {
+    let stringToCheck = string.trim().replaceAll(" ", "").toLowerCase();
+    let reversedString = stringToCheck.split("").reverse().join("");
+    if (stringToCheck === reversedString) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
 module.exports = {
     getInitials,
-    createSlug
+    createSlug,
+    average,
+    isPalindrome
 }
--- testsnacks.test.js
+++ testsnacks.test.js
@@ -1,4 +1,4 @@
-const { getInitials, createSlug } = require('./testsnacks.js')
+const { getInitials, createSlug, average, isPalindrome } = require('./testsnacks.js')
 
 // 🏆 Snack 1
 // Creare un test che verifichi la seguente descrizione:
@@ -10,6 +10,7 @@ test("La funzione getInitials restituisce le iniziali di un nome completo.", () 
     expect(getInitials("nicola argiolas")).toBe("NA")
 });
 
+
 // 🏆 Snack 2
 // Creare un test che verifichi la seguente descrizione:
 // 👉 "La funzione createSlug restituisce una stringa in lowercase."
@@ -19,3 +20,34 @@ test("La funzione createSlug restituisce una stringa in lowercase.", () => {
     expect(createSlug("NON FUNZIONA PORCA PUTTANA")).toBe("non-funziona-porca-puttana");
     expect(createSlug("Ok Finalmente Ora Funziona")).toBe("ok-finalmente-ora-funziona");
 });
+
+
+// 🏆 Snack 3
+// Creare un test che verifichi la seguente descrizione:
+// 👉 "La funzione average calcola la media aritmetica di un array di numeri."
+
+test("La funzione average calcola la media aritmetica di un array di numeri.", () => {
+    expect(average([1, 2, 3, 4, 5])).toBe(3);
+    expect(average([2, 2, 10, 18, 18])).toBe(10);
+    expect(average([0, 5, 5, 10])).toBe(5);
+})
+
+// 🏆 Snack 4
+// Creare un test che verifichi la seguente descrizione:
+// 👉 "La funzione createSlug sostituisce gli spazi con -."
+// 📌 Esempi:
+// createSlug("Questo è un test") → "questo-e-un-test"
+
+// Già fatto nello snack 2
+
+
+// 🏆 Snack 5
+// Creare un test che verifichi la seguente descrizione:
+// 👉 "La funzione isPalindrome verifica se una stringa è un palindromo."
+
+test("La funzione isPalindrome verifica se una stringa è un palindromo.", () => {
+    expect(isPalindrome("otto")).toBeTruthy();
+    expect(isPalindrome("jest")).toBeFalsy();
+    expect(isPalindrome("Anna")).toBeTruthy();
+    expect(isPalindrome("I Topi Non Avevano Nipoti")).toBeTruthy();
+})
